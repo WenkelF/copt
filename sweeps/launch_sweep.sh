01533580e66b0117ad308c66cc4caf07db7e145e
WenkelF <frederik.wenkel@login-4.server.mila.quebec>
2023-11-22 14:13:54 -0500

    Updating wandb sweep configs and adding sbatch scripts for mila cluster

---
+++ sweeps/launch_sweep.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+#SBATCH --array=1-10
+
+## Name of your SLURM job
+# SBATCH --job-name=run
+
+## Files for logs: here we redirect stoout and sterr to the same file
+#SBATCH --output=outputs/out_%x_%j_%a.out   # %x=job-name, %j=jobid, %a=array-id
+#SBATCH --error=outputs/error_%x_%j_%a.out
+#SBATCH --open-mode=append
+
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+
+## Time limit for the job
+#SBATCH --time=3:00:00
+
+#SBATCH --mem=10Gb
+
+## Number of GPUs to use
+#SBATCH --gres=gpu:1
+
+## Partition to use,
+#SBATCH --partition=long
+
+set -e
+
+cd /home/mila/f/frederik.wenkel/projects/copt_graphgym
+
+module load miniconda/3 cuda/11.8
+
+source /home/mila/f/frederik.wenkel/.bashrc
+
+conda activate /home/mila/f/frederik.wenkel/miniconda3/envs/copt
+
+wandb agent --count 1 wenkelf/copt_graphgym/baqdnkxo
