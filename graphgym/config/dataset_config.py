c1cf6c7e3b691590dfeb2a4d95b43d08fd0e6308
Semih Cantürk <=>
2023-12-19 19:45:07 -0500

    refactor default configs for synthetic datasets

--- graphgym/config/dataset_config.py
+++ graphgym/config/dataset_config.py
@@ -31,8 +31,9 @@ def dataset_cfg(cfg):
     cfg.dataset.umg_random_seed = 0  # for random indexing
 
     cfg.dataset.set_graph_stats = False
+    cfg.dataset.graph_stats = ['degree', 'eccentricity', 'cluster_coefficient', 'triangle_count']
 
-    cfg.dataset.multiprocessing = False
+    cfg.dataset.multiprocessing = True
     cfg.dataset.num_workers = 4
 
     cfg.dataset.label = True
@@ -65,38 +66,26 @@ def er_cfg(cfg):
 
 @register_config('er_50_02_cfg')
 def er_50_02_cfg(cfg):
-    """Configuration options for nx datasets.
-    """
-    cfg.er_50_02 = CN()
-    cfg.er_50_02.num_samples = 10000
-    cfg.er_50_02.n_min = 30
-    cfg.er_50_02.n_max = 50
-    cfg.er_50_02.p = 0.2
-    cfg.er_50_02.supp_mtx = ["edge_index"]
+    cfg.er.v50_02 = CN()
+    cfg.er.v50_02.n_min = 30
+    cfg.er.v50_02.n_max = 50
+    cfg.er.v50_02.p = 0.2
 
 
 @register_config('er_50_03_cfg')
 def er_50_03_cfg(cfg):
-    """Configuration options for nx datasets.
-    """
-    cfg.er_50_03 = CN()
-    cfg.er_50_03.num_samples = 10000
-    cfg.er_50_03.n_min = 30
-    cfg.er_50_03.n_max = 50
-    cfg.er_50_03.p = 0.3
-    cfg.er_50_03.supp_mtx = ["edge_index"]
+    cfg.er.v50_03 = CN()
+    cfg.er.v50_03.n_min = 30
+    cfg.er.v50_03.n_max = 50
+    cfg.er.v50_03.p = 0.3
 
 
 @register_config('er_50_04_cfg')
 def er_50_04_cfg(cfg):
-    """Configuration options for nx datasets.
-    """
-    cfg.er_50_04 = CN()
-    cfg.er_50_04.num_samples = 10000
-    cfg.er_50_04.n_min = 30
-    cfg.er_50_04.n_max = 50
-    cfg.er_50_04.p = 0.4
-    cfg.er_50_04.supp_mtx = ["edge_index"]
+    cfg.er.v50_04 = CN()
+    cfg.er.v50_04.n_min = 30
+    cfg.er.v50_04.n_max = 50
+    cfg.er.v50_04.p = 0.4
 
 
 @register_config('bp_cfg')
@@ -106,121 +95,123 @@ def bp_cfg(cfg):
     cfg.bp = CN()
     cfg.bp.num_samples = 10000
     cfg.bp.mean = 10
-    cfg.bp.n_min = 4
-    cfg.bp.n_max = 20
-    cfg.bp.p_edge_bp = 0.4
+    cfg.bp.n_min = 10
+    cfg.bp.n_max = 30
+    cfg.bp.p_edge_bp = 0.3
     cfg.bp.p_edge_er = 0.1
     cfg.bp.supp_mtx = ["edge_index"]
 
 
 @register_config('bp_20_00_cfg')
 def bp_20_00_cfg(cfg):
-    """Configuration options for nx datasets.
-    """
-    cfg.bp_20_00 = CN()
-    cfg.bp_20_00.num_samples = 10000
-    cfg.bp_20_00.mean = 20
-    cfg.bp_20_00.n_min = 10
-    cfg.bp_20_00.n_max = 30
-    cfg.bp_20_00.p_edge_bp = 0.3
-    cfg.bp_20_00.p_edge_er = 0.0
-    cfg.bp_20_00.supp_mtx = ["edge_index"]
+    cfg.bp.v20_01 = CN()
+    cfg.bp.v20_01.mean = 20
+    cfg.bp.v20_01.p_edge_er = 0.0
 
 
 @register_config('bp_20_01_cfg')
 def bp_20_01_cfg(cfg):
-    """Configuration options for nx datasets.
-    """
-    cfg.bp_20_01 = CN()
-    cfg.bp_20_01.num_samples = 10000
-    cfg.bp_20_01.mean = 20
-    cfg.bp_20_01.n_min = 10
-    cfg.bp_20_01.n_max = 30
-    cfg.bp_20_01.p_edge_bp = 0.3
-    cfg.bp_20_01.p_edge_er = 0.1
-    cfg.bp_20_01.supp_mtx = ["edge_index"]
+    cfg.bp.v20_01 = CN()
+    cfg.bp.v20_01.mean = 20
+    cfg.bp.v20_01.p_edge_er = 0.1
 
 
 @register_config('bp_20_02_cfg')
 def bp_20_02_cfg(cfg):
-    """Configuration options for nx datasets.
-    """
-    cfg.bp_20_02 = CN()
-    cfg.bp_20_02.num_samples = 10000
-    cfg.bp_20_02.mean = 20
-    cfg.bp_20_02.n_min = 10
-    cfg.bp_20_02.n_max = 30
-    cfg.bp_20_02.p_edge_bp = 0.3
-    cfg.bp_20_02.p_edge_er = 0.2
-    cfg.bp_20_02.supp_mtx = ["edge_index"]
+    cfg.bp.v20_02 = CN()
+    cfg.bp.v20_02.mean = 20
+    cfg.bp.v20_02.p_edge_er = 0.2
 
 
 @register_config('bp_20_03_cfg')
 def bp_20_03_cfg(cfg):
-    """Configuration options for nx datasets.
-    """
-    cfg.bp_20_03 = CN()
-    cfg.bp_20_03.num_samples = 10000
-    cfg.bp_20_03.mean = 20
-    cfg.bp_20_03.n_min = 10
-    cfg.bp_20_03.n_max = 30
-    cfg.bp_20_03.p_edge_bp = 0.3
-    cfg.bp_20_03.p_edge_er = 0.3
-    cfg.bp_20_03.supp_mtx = ["edge_index"]
+    cfg.bp.v20_03 = CN()
+    cfg.bp.v20_03.mean = 20
+    cfg.bp.v20_03.p_edge_er = 0.3
 
 
-@register_config('pc_test_cfg')
-def pc_test_cfg(cfg):
+@register_config('rb_cfg')
+def rb_cfg(cfg):
     """Configuration options for nx datasets.
     """
-    cfg.pc_test = CN()
-    cfg.pc_test.num_samples = 1000
-    cfg.pc_test.graph_size = 500
-    cfg.pc_test.clique_size = None
-    cfg.pc_test.supp_mtx = ["edge_index"]
+    cfg.rb = CN()
+    # features can be one of ['node_const', 'node_onehot', 'node_clustering_coefficient', 'node_pagerank']
+    cfg.rb.num_samples = 4500
+
+
+@register_config('rb_small_cfg')
+def rb_small_cfg(cfg):
+    """Configuration options for nx datasets.
+    """
+    cfg.rb.small = CN()
+    cfg.rb.small.num_samples = 4500
+    cfg.rb.small.n = (200, 300)
+    cfg.rb.small.na = (20, 25)
+    cfg.rb.small.k = (5, 12)
+
+
+@register_config('rb_large_cfg')
+def rb_large_cfg(cfg):
+    """Configuration options for nx datasets.
+    """
+    cfg.rb.large = CN()
+    cfg.rb.large.num_samples = 4500
+    cfg.rb.large.n = (800, 1200)
+    cfg.rb.large.na = (40, 55)
+    cfg.rb.large.k = (20, 25)
+
+@register_config('pc_cfg')
+def pc_cfg(cfg):
+    """Configuration options for nx datasets.
+    """
+    cfg.pc = CN()
+    cfg.pc.num_samples = 1000
+    cfg.pc.graph_size = 500
+    cfg.pc.clique_size = None
+    cfg.pc.supp_mtx = ["edge_index"]
 
 
 @register_config('pc_500_20_cfg')
 def pc_500_20_cfg(cfg):
     """Configuration options for nx datasets.
     """
-    cfg.pc_500_20 = CN()
-    cfg.pc_500_20.num_samples = 10000
-    cfg.pc_500_20.graph_size = 500
-    cfg.pc_500_20.clique_size = None
-    cfg.pc_500_20.supp_mtx = ["edge_index"]
+    cfg.pc.v500_20 = CN()
+    cfg.pc.v500_20.graph_size = 500
+    cfg.pc.v500_20.clique_size = None
 
 
 @register_config('pc_100_40_cfg')
 def pc_100_40_cfg(cfg):
     """Configuration options for nx datasets.
     """
-    cfg.pc_100_40 = CN()
-    cfg.pc_100_40.num_samples = 10000
-    cfg.pc_100_40.graph_size = 100
-    cfg.pc_100_40.clique_size = 40
-    cfg.pc_100_40.supp_mtx = ["edge_index"]
+    cfg.pc.v100_40 = CN()
+    cfg.pc.v100_40.graph_size = 100
+    cfg.pc.v100_40.clique_size = 40
+
+
+@register_config('ba_cfg')
+def ba_cfg(cfg):
+    cfg.ba = CN()
+    cfg.ba.num_samples = 5000
+    cfg.ba.n_min = 200
+    cfg.ba.n_max = 300
+    cfg.ba.num_edges = 4
+    cfg.ba.supp_mtx = ["edge_index"]
 
 
 @register_config('ba_small_cfg')
 def ba_small_cfg(cfg):
     """Configuration options for nx datasets.
     """
-    cfg.ba_small = CN()
-    cfg.ba_small.num_samples = 5000
-    cfg.ba_small.n_min = 200
-    cfg.ba_small.n_max = 300
-    cfg.ba_small.num_edges = 4
-    cfg.ba_small.supp_mtx = ["edge_index"]
+    cfg.ba.small = CN()
+    cfg.ba.small.n_min = 200
+    cfg.ba.small.n_max = 300
 
 
 @register_config('ba_large_cfg')
 def ba_large_cfg(cfg):
     """Configuration options for nx datasets.
     """
-    cfg.ba_large = CN()
-    cfg.ba_large.num_samples = 5000
-    cfg.ba_large.n_min = 800
-    cfg.ba_large.n_max = 1200
-    cfg.ba_large.num_edges = 4
-    cfg.ba_large.supp_mtx = ["edge_index"]
+    cfg.ba.large = CN()
+    cfg.ba.large.n_min = 800
+    cfg.ba.large.n_max = 1200
